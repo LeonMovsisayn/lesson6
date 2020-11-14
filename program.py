c50594b0dae60a9b48c5336fdfff92745cd358d5
Leon Movsisyan <mleonk1417@gmail.com>
2020-11-14 14:28:49 +0300

    Add new route

--- program.py
+++ program.py
@@ -8,3 +8,7 @@ def hello_world():
 @app.route('/v2')
 def v2():
     return 'Second action'
+
+@app.route('/movsisyan')
+def movsisyan():
+    return 'Hello from CI with GitHub Actions by Movsisyan'
